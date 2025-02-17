35e9319c480fafbb75fbf1d0f162f223ad3cf242
spectralo <arthur@spectralo.me>
2025-02-17 20:01:05 +0100

    i wonder why tailwind is broken :/

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,5 +2,9 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 fn main() {
+    tauri::Builder::default()
+        .plugin(tauri_plugin_os::init())
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
     juice_offline_lib::run()
 }
